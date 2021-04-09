849b0ef98c0f97d37959e288fe428389c142171f
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-04-09 16:59:10 -0600

    Created ViewModel and view for burial summar, and recreated the burial controller so Tingyu can push the changes

--- Models/ViewModels/IndexViewModel.cs
+++ Models/ViewModels/IndexViewModel.cs
@@ -7,5 +7,11 @@ namespace DigginPharoh.Models.ViewModels
 {
     public class IndexViewModel
     {
+        public List<Burial> BurialList { get; set; }
+        public List<BurialIDInfo> BurialIDInfoList { get; set; }
+        public List<BiologicalSamples> BiologicalSampleList { get; set; }
+        public List<Carbon_Dating> Carbon_DatingList { get; set; }
+        public List<Cranial> CranialList { get; set; }
+        public List<Note> NoteList { get; set; }
     }
 }
