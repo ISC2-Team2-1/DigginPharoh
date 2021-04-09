364fd2373cdf4e7110194ba5dfe50f09b9583524
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-04-09 11:52:02 -0600

    added the rest of the model scaffolding

---
+++ Controllers/Carbon_DatingController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DigginPharoh.Data;
+using DigginPharoh.Models;
+
+namespace DigginPharoh.Controllers
+{
+    public class Carbon_DatingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Carbon_DatingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Carbon_Dating
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.CarbonDates.ToListAsync());
+        }
+
+        // GET: Carbon_Dating/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carbon_Dating = await _context.CarbonDates
+                .FirstOrDefaultAsync(m => m.Burial_Id == id);
+            if (carbon_Dating == null)
+            {
+                return NotFound();
+            }
+
+            return View(carbon_Dating);
+        }
+
+        // GET: Carbon_Dating/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Carbon_Dating/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Burial_Id,AREA_Num,Rack_Num,TUBE_Num,Description,Size_ml,Foci,C14_Sample_2017,Location,Question,Conventional_14C_Age_BP,Calendar_Date_14C,Calibrated_95_Calendar_Date_MAX,Calibrated_95_Calendar_Date_MIN,Calibrated_95_Calendar_Date_SPAN,Calibrated_95_Calendar_Date_AVG,Category,Notes")] Carbon_Dating carbon_Dating)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(carbon_Dating);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carbon_Dating);
+        }
+
+        // GET: Carbon_Dating/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carbon_Dating = await _context.CarbonDates.FindAsync(id);
+            if (carbon_Dating == null)
+            {
+                return NotFound();
+            }
+            return View(carbon_Dating);
+        }
+
+        // POST: Carbon_Dating/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Burial_Id,AREA_Num,Rack_Num,TUBE_Num,Description,Size_ml,Foci,C14_Sample_2017,Location,Question,Conventional_14C_Age_BP,Calendar_Date_14C,Calibrated_95_Calendar_Date_MAX,Calibrated_95_Calendar_Date_MIN,Calibrated_95_Calendar_Date_SPAN,Calibrated_95_Calendar_Date_AVG,Category,Notes")] Carbon_Dating carbon_Dating)
+        {
+            if (id != carbon_Dating.Burial_Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(carbon_Dating);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!Carbon_DatingExists(carbon_Dating.Burial_Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carbon_Dating);
+        }
+
+        // GET: Carbon_Dating/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carbon_Dating = await _context.CarbonDates
+                .FirstOrDefaultAsync(m => m.Burial_Id == id);
+            if (carbon_Dating == null)
+            {
+                return NotFound();
+            }
+
+            return View(carbon_Dating);
+        }
+
+        // POST: Carbon_Dating/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var carbon_Dating = await _context.CarbonDates.FindAsync(id);
+            _context.CarbonDates.Remove(carbon_Dating);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool Carbon_DatingExists(string id)
+        {
+            return _context.CarbonDates.Any(e => e.Burial_Id == id);
+        }
+    }
+}
---
+++ Controllers/CranialsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DigginPharoh.Data;
+using DigginPharoh.Models;
+
+namespace DigginPharoh.Controllers
+{
+    public class CranialsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CranialsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cranials
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Craniums.ToListAsync());
+        }
+
+        // GET: Cranials/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cranial = await _context.Craniums
+                .FirstOrDefaultAsync(m => m.Burial_Id == id);
+            if (cranial == null)
+            {
+                return NotFound();
+            }
+
+            return View(cranial);
+        }
+
+        // GET: Cranials/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cranials/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Burial_Id,Burial_Depth,Sample_Number,Maximum_Cranial_Length,Maximum_Cranial_Breadth,Basion_Bregma_Height,Basion_Nasion,Basion_Prosthion_Length,Bizygomatic_Diameter,Nasion_Prosthion,Maximum_Nasal_Breadth,Interorbital_Breadth,Burial_Artifact_Description,Buried_with_Artifacts,Giles_Gender,Body_Gender")] Cranial cranial)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cranial);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cranial);
+        }
+
+        // GET: Cranials/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cranial = await _context.Craniums.FindAsync(id);
+            if (cranial == null)
+            {
+                return NotFound();
+            }
+            return View(cranial);
+        }
+
+        // POST: Cranials/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Burial_Id,Burial_Depth,Sample_Number,Maximum_Cranial_Length,Maximum_Cranial_Breadth,Basion_Bregma_Height,Basion_Nasion,Basion_Prosthion_Length,Bizygomatic_Diameter,Nasion_Prosthion,Maximum_Nasal_Breadth,Interorbital_Breadth,Burial_Artifact_Description,Buried_with_Artifacts,Giles_Gender,Body_Gender")] Cranial cranial)
+        {
+            if (id != cranial.Burial_Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cranial);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CranialExists(cranial.Burial_Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cranial);
+        }
+
+        // GET: Cranials/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cranial = await _context.Craniums
+                .FirstOrDefaultAsync(m => m.Burial_Id == id);
+            if (cranial == null)
+            {
+                return NotFound();
+            }
+
+            return View(cranial);
+        }
+
+        // POST: Cranials/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var cranial = await _context.Craniums.FindAsync(id);
+            _context.Craniums.Remove(cranial);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CranialExists(string id)
+        {
+            return _context.Craniums.Any(e => e.Burial_Id == id);
+        }
+    }
+}
