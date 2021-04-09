93ab1dff13f8d2c1618e5706127d191d3132182d
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-04-08 22:09:33 -0600

    set up burial to include waht was left of additional notes and removed the other class

--- Models/Burial.cs
+++ Models/Burial.cs
@@ -81,5 +81,27 @@ namespace DigginPharoh.Models
         public int year_found { get; set; }
         public string month_found { get; set; }
         public int day_found { get; set; }
+        public string Field_Book { get; set; }
+        public int Field_Book_Page_Number { get; set; }
+        public bool Skull_At_Magazine { get; set; }
+        public bool Postcrania_At_Magazine { get; set; }
+        public string Rack_And_Shelf { get; set; }
+        public bool To_Be_Confirmed { get; set; }
+        public bool Skull_Trauma { get; set; }
+        public bool Postcrania_Trauma { get; set; }
+        public bool Cribra_Orbitala { get; set; }
+        public string Porotic_Hyperostosis { get; set; }
+        public string Porotic_Hyperostosis_Locations { get; set; }
+        public bool Metopic_Suture { get; set; }
+        public bool Button_Osteoma { get; set; }
+        public string Osteology_Unknown_Comment { get; set; }
+        public bool Temporal_Mandibular_Joint_Osteoarthritis { get; set; }
+        public bool Linear_Hypoplasia_Enamel { get; set; }
+        public int Area_Hill_Burials { get; set; }
+        public int Tomb { get; set; }
+        public string Goods { get; set; }
+        public string Cluster { get; set; }
+        public string Face_Bundle { get; set; }
+        public int Body_Analysis_Year { get; set; }
     }
 }
