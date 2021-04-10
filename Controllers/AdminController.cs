b0adeaf9451ce78a0e81e1569a3bf2622a658541
Tyler Barty <tyler.barty@gmail.com>
2021-04-09 21:40:47 -0600

    backtrack fixes

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -55,32 +55,32 @@ namespace DigginPharoh.Controllers
             return View(users);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                // We just need to specify a unique role name to create a new role
-                IdentityRole identityRole = new IdentityRole
-                {
-                    Name = model.RoleName
-                };
+        //[HttpPost]
+        //public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        // We just need to specify a unique role name to create a new role
+        //        IdentityRole identityRole = new IdentityRole
+        //        {
+        //            Name = model.RoleName
+        //        };
 
-                // Saves the role in the underlying AspNetRoles table
-                IdentityResult result = await roleManager.CreateAsync(identityRole);
+        //        // Saves the role in the underlying AspNetRoles table
+        //        IdentityResult result = await roleManager.CreateAsync(identityRole);
 
-                if (result.Succeeded)
-                {
-                    return RedirectToAction("index", "home");
-                }
+        //        if (result.Succeeded)
+        //        {
+        //            return RedirectToAction("index", "home");
+        //        }
 
-                foreach (IdentityError error in result.Errors)
-                {
-                    ModelState.AddModelError("", error.Description);
-                }
-            }
+        //        foreach (IdentityError error in result.Errors)
+        //        {
+        //            ModelState.AddModelError("", error.Description);
+        //        }
+        //    }
 
-            return View(model);
-        }
+        //    return View(model);
+        //}
     }
 }
