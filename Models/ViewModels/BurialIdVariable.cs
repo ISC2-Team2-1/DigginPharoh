7beaa2d00f28f2e1ce322f54f04cbebfc935bd7a
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-04-10 22:19:37 -0600

    form update, id passing troubleshooting

---
+++ Models/ViewModels/BurialIdVariable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigginPharoh.Models.ViewModels
+{
+    public class BurialIdVariable
+    {
+        public string BurialIdVar { get; set; }
+    }
+}
