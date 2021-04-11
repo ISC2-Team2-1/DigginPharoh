d989416df424635689c2144ee88c85e4a5685651
Tyler Barty <tyler.barty@gmail.com>
2021-04-10 18:56:39 -0600

    Page Filter Additions

--- Models/Filters.cs
+++ Models/Filters.cs
@@ -12,12 +12,30 @@ namespace DigginPharoh.Models
             FilterString = filterstring ?? "all";
             string[] filters = FilterString.Split('-');
             HeadDirection = filters[0];
+            Depth = int.Parse(filters[1]);
+            SexGE = filters[2];
+            GenderBodCol = filters[3];
+            Preservation = filters[4];
+            YearFound = filters[5];
+
         }
 
         public string FilterString { get; }
         public string HeadDirection { get; }
+        public int Depth { get; }
+        public string SexGE { get; }
+        public string GenderBodCol { get; }
+        public string Preservation { get;}
+        public string YearFound { get; }
+
 
         public bool HasDirection => HeadDirection.ToLower() != "all";
+        public bool HasDepth => Depth != -1;
+        public bool HasSexGe => SexGE.ToLower() != "all";
+        public bool HasGenderBodCol => GenderBodCol.ToLower() != "all";
+        public bool HasPreservation => Preservation.ToLower() != "all";
+        public bool HasYear => YearFound.ToLower() != "all";
+
 
         public static Dictionary<string, string> HeadDirFilterValues =>
         new Dictionary<string, string>
