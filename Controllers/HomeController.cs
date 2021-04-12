0dc8c128806ac332a3b7c0c636bfd599c4c5404e
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-04-12 13:01:56 -0600

    dropdown leads to next page of form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -243,30 +243,6 @@ namespace DigginPharoh.Controllers
             return RedirectToAction("BurialSummary", new { ID = id });
         }
 
-
-        // GET: Burials/Create
-        public IActionResult Create(string burialid)
-        {
-            ViewBag.BurialId = burialid;
-            return View();
-        }
-
-        // POST: Burials/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Burial_Id,Gamous_Id,burial_depth,WESTTOHEAD,WESTTOFEET,SOUTHTOHEAD,SOUTHTOFEET,Preservation,Burial_Situation,head_direction,Adult_Child,estimate_age,AGEMETHOD,gender_body_col,Sex_Gender_GE,SEXMETHOD,GE_function_total,length_of_remains,sample_number,basilar_suture,ventral_arc,subpubic_angle,sciatic_notch,pubic_bone,preaur_sulcus,medial_IP_ramus,dorsal_pitting,femur_head,humerus_head,osteophytosis,pubic_symphysis,femur_length,humerus_length,tibia_length,robust,supraorbital_ridges,orbit_edge,parietal_bossing,gonian,nuchal_crest,zygomatic_crest,cranial_suture,maximum_cranial_length,maximum_cranial_breadth,basion_bregma_height,basion_nasion,basion_prosthion_length,bizygomatic_diameter,nasion_prosthion,maximum_nasal_breadth,interorbital_breadth,artifacts_description,hair_color,hair_taken,soft_tissue_taken,bone_taken,tooth_taken,textile_taken,SAMPLE,description_of_taken,artifact_found,estimate_living_stature,tooth_attrition,tooth_eruption,pathology_anomalies,epiphyseal_union,year_found,month_found,day_found,Field_Book,Field_Book_Page_Number,Skull_At_Magazine,Postcrania_At_Magazine,Rack_And_Shelf,To_Be_Confirmed,Skull_Trauma,Postcrania_Trauma,Cribra_Orbitala,Porotic_Hyperostosis,Porotic_Hyperostosis_Locations,Metopic_Suture,Button_Osteoma,Osteology_Unknown_Comment,Temporal_Mandibular_Joint_Osteoarthritis,Linear_Hypoplasia_Enamel,Area_Hill_Burials,Tomb,Goods,Cluster,Face_Bundle,Body_Analysis_Year")] Burial burial)
-        {
-            if (ModelState.IsValid)
-            {
-                context.Add(burial);
-                await context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(burial);
-        }
-
         public IActionResult BurialDetails(string detailId)
         {
             return View(new IndexViewModel
@@ -338,37 +314,60 @@ namespace DigginPharoh.Controllers
 
                 if (NextPage == "Burial")
                 {
-                    return RedirectToAction("IndexId", new { ID = burialIdHolder });
+                    return RedirectToAction("Create", new { ID = burialIdHolder });
                 }
 
                 if (NextPage == "Biological Sample")
                 {
-                    return RedirectToAction("IndexId", new { ID = burialIdHolder });
+                    return RedirectToAction("CreateBioSample", new { ID = burialIdHolder });
                 }
 
                 if (NextPage == "Cranial")
                 {
-                    return RedirectToAction("IndexId", new { ID = burialIdHolder });
+                    return RedirectToAction("CreateCranial", new { ID = burialIdHolder });
                 }
 
                 if (NextPage == "Carbon Dating")
                 {
-                    return RedirectToAction("IndexId", new { ID = burialIdHolder });
+                    return RedirectToAction("CreateCarbonDating", new { ID = burialIdHolder });
                 }
 
                 if (NextPage == "Note")
                 {
-                    return RedirectToAction("IndexId", new { ID = burialIdHolder });
+                    return RedirectToAction("CreateNote", new { ID = burialIdHolder });
                 }
             }
 
             return View(burialIDInfo); // make error page
         }
 
-        // GET: Cranials/Create
-        public IActionResult CreateCranial(string burialId)
+        // GET: Burials/Create
+        public IActionResult Create(string id)
         {
-            ViewBag.BurialId = burialId;
+            ViewBag.BurialId = id;
+            return View();
+        }
+
+        // POST: Burials/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Burial_Id,Gamous_Id,burial_depth,WESTTOHEAD,WESTTOFEET,SOUTHTOHEAD,SOUTHTOFEET,Preservation,Burial_Situation,head_direction,Adult_Child,estimate_age,AGEMETHOD,gender_body_col,Sex_Gender_GE,SEXMETHOD,GE_function_total,length_of_remains,sample_number,basilar_suture,ventral_arc,subpubic_angle,sciatic_notch,pubic_bone,preaur_sulcus,medial_IP_ramus,dorsal_pitting,femur_head,humerus_head,osteophytosis,pubic_symphysis,femur_length,humerus_length,tibia_length,robust,supraorbital_ridges,orbit_edge,parietal_bossing,gonian,nuchal_crest,zygomatic_crest,cranial_suture,maximum_cranial_length,maximum_cranial_breadth,basion_bregma_height,basion_nasion,basion_prosthion_length,bizygomatic_diameter,nasion_prosthion,maximum_nasal_breadth,interorbital_breadth,artifacts_description,hair_color,hair_taken,soft_tissue_taken,bone_taken,tooth_taken,textile_taken,SAMPLE,description_of_taken,artifact_found,estimate_living_stature,tooth_attrition,tooth_eruption,pathology_anomalies,epiphyseal_union,year_found,month_found,day_found,Field_Book,Field_Book_Page_Number,Skull_At_Magazine,Postcrania_At_Magazine,Rack_And_Shelf,To_Be_Confirmed,Skull_Trauma,Postcrania_Trauma,Cribra_Orbitala,Porotic_Hyperostosis,Porotic_Hyperostosis_Locations,Metopic_Suture,Button_Osteoma,Osteology_Unknown_Comment,Temporal_Mandibular_Joint_Osteoarthritis,Linear_Hypoplasia_Enamel,Area_Hill_Burials,Tomb,Goods,Cluster,Face_Bundle,Body_Analysis_Year")] Burial burial)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(burial);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(burial);
+        }
+
+        // GET: Cranials/Create
+        public IActionResult CreateCranial(string id)
+        {
+            ViewBag.BurialId = id;
             return View();
         }
 
@@ -389,9 +388,9 @@ namespace DigginPharoh.Controllers
         }
 
         // GET: BiologicalSamples/Create
-        public IActionResult CreateBioSample(string burialId)
+        public IActionResult CreateBioSample(string id)
         {
-            ViewBag.BurialId = burialId;
+            ViewBag.BurialId = id;
             return View();
         }
 
@@ -412,9 +411,9 @@ namespace DigginPharoh.Controllers
         }
 
         // GET: CarbonDating/Create
-        public IActionResult CreateCarbonDating(string burialId)
+        public IActionResult CreateCarbonDating(string id)
         {
-            ViewBag.BurialId = burialId;
+            ViewBag.BurialId = id;
             return View();
         }
 
@@ -436,9 +435,9 @@ namespace DigginPharoh.Controllers
 
 
         // GET: Notes/Create
-        public IActionResult CreateNote(string burialId)
+        public IActionResult CreateNote(string id)
         {
-            ViewBag.BurialId = burialId;
+            ViewBag.BurialId = id;
             return View();
         }
 
