c3e9433c7cd20b481868e4d5e0013fa0399c64ba
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-04-12 15:04:00 -0600

    added select new record to add to same id to Creat burial page and functino in index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -355,12 +355,54 @@ namespace DigginPharoh.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Burial_Id,Gamous_Id,burial_depth,WESTTOHEAD,WESTTOFEET,SOUTHTOHEAD,SOUTHTOFEET,Preservation,Burial_Situation,head_direction,Adult_Child,estimate_age,AGEMETHOD,gender_body_col,Sex_Gender_GE,SEXMETHOD,GE_function_total,length_of_remains,sample_number,basilar_suture,ventral_arc,subpubic_angle,sciatic_notch,pubic_bone,preaur_sulcus,medial_IP_ramus,dorsal_pitting,femur_head,humerus_head,osteophytosis,pubic_symphysis,femur_length,humerus_length,tibia_length,robust,supraorbital_ridges,orbit_edge,parietal_bossing,gonian,nuchal_crest,zygomatic_crest,cranial_suture,maximum_cranial_length,maximum_cranial_breadth,basion_bregma_height,basion_nasion,basion_prosthion_length,bizygomatic_diameter,nasion_prosthion,maximum_nasal_breadth,interorbital_breadth,artifacts_description,hair_color,hair_taken,soft_tissue_taken,bone_taken,tooth_taken,textile_taken,SAMPLE,description_of_taken,artifact_found,estimate_living_stature,tooth_attrition,tooth_eruption,pathology_anomalies,epiphyseal_union,year_found,month_found,day_found,Field_Book,Field_Book_Page_Number,Skull_At_Magazine,Postcrania_At_Magazine,Rack_And_Shelf,To_Be_Confirmed,Skull_Trauma,Postcrania_Trauma,Cribra_Orbitala,Porotic_Hyperostosis,Porotic_Hyperostosis_Locations,Metopic_Suture,Button_Osteoma,Osteology_Unknown_Comment,Temporal_Mandibular_Joint_Osteoarthritis,Linear_Hypoplasia_Enamel,Area_Hill_Burials,Tomb,Goods,Cluster,Face_Bundle,Body_Analysis_Year")] Burial burial)
+        public async Task<IActionResult> Create([Bind("Burial_Id,Gamous_Id,burial_depth,WESTTOHEAD,WESTTOFEET,SOUTHTOHEAD,SOUTHTOFEET,Preservation,Burial_Situation,head_direction,Adult_Child,estimate_age,AGEMETHOD,gender_body_col,Sex_Gender_GE,SEXMETHOD,GE_function_total,length_of_remains,sample_number,basilar_suture,ventral_arc,subpubic_angle,sciatic_notch,pubic_bone,preaur_sulcus,medial_IP_ramus,dorsal_pitting,femur_head,humerus_head,osteophytosis,pubic_symphysis,femur_length,humerus_length,tibia_length,robust,supraorbital_ridges,orbit_edge,parietal_bossing,gonian,nuchal_crest,zygomatic_crest,cranial_suture,maximum_cranial_length,maximum_cranial_breadth,basion_bregma_height,basion_nasion,basion_prosthion_length,bizygomatic_diameter,nasion_prosthion,maximum_nasal_breadth,interorbital_breadth,artifacts_description,hair_color,hair_taken,soft_tissue_taken,bone_taken,tooth_taken,textile_taken,SAMPLE,description_of_taken,artifact_found,estimate_living_stature,tooth_attrition,tooth_eruption,pathology_anomalies,epiphyseal_union,year_found,month_found,day_found,Field_Book,Field_Book_Page_Number,Skull_At_Magazine,Postcrania_At_Magazine,Rack_And_Shelf,To_Be_Confirmed,Skull_Trauma,Postcrania_Trauma,Cribra_Orbitala,Porotic_Hyperostosis,Porotic_Hyperostosis_Locations,Metopic_Suture,Button_Osteoma,Osteology_Unknown_Comment,Temporal_Mandibular_Joint_Osteoarthritis,Linear_Hypoplasia_Enamel,Area_Hill_Burials,Tomb,Goods,Cluster,Face_Bundle,Body_Analysis_Year")] Burial burial, string id)
         {
             if (ModelState.IsValid)
             {
+                //context.Add(burial);
+                //await context.SaveChangesAsync();
+                //return RedirectToAction(nameof(Index));
+
+                //Saves next step the user selected, whether they will create an additional record for this ID or not
+                var nextStep = burial.Burial_Id;
+                //Cleans Id passed through in route, then saves it to the note object to be addded to the database
+                string cleanId = id.Replace("%2F", "/");
+                burial.Burial_Id = cleanId;
+                //Adds note to context
                 context.Add(burial);
                 await context.SaveChangesAsync();
+                //Redirects based on the user's  nextStep selection
+                //Return to index
+                if (nextStep == "Not now")
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                //Yes, a Burial
+                if (nextStep == "Yes, a Burial")
+                {
+                    return RedirectToAction("Create", new { ID = cleanId });
+                }
+                //Yes, a Biological Sample
+                if (nextStep == "Yes, a Biological Sample")
+                {
+                    return RedirectToAction("CreateBioSample", new { ID = cleanId });
+                }
+                //Yes, a Cranial Record
+                if (nextStep == "Yes, a Cranial Record")
+                {
+                    return RedirectToAction("CreateCranial", new { ID = cleanId });
+                }
+                //Yes, a Carbon Dating Record
+                if (nextStep == "Yes, a Carbon Dating Record")
+                {
+                    return RedirectToAction("CreateCarbonDating", new { ID = cleanId });
+                }
+                //Yes, a Note
+                if (nextStep == "Yes, a Note")
+                {
+                    return RedirectToAction("CreateNote", new { ID = cleanId });
+                }
+
                 return RedirectToAction(nameof(Index));
             }
             return View(burial);
@@ -384,10 +426,6 @@ namespace DigginPharoh.Controllers
         {
             if (ModelState.IsValid)
             {
-                //context.Add(cranial);
-                //await context.SaveChangesAsync();
-                //return RedirectToAction(nameof(Index));
-
                 //Saves next step the user selected, whether they will create an additional record for this ID or not
                 var nextStep = cranial.Burial_Id;
                 //Cleans Id passed through in route, then saves it to the note object to be addded to the database
@@ -430,7 +468,7 @@ namespace DigginPharoh.Controllers
 
                 return RedirectToAction(nameof(Index));
             }
-            return View(cranial);
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: BiologicalSamples/Create
